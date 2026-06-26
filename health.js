4180fa68e4ec74b50559abac5c7d8b9443569cb4
GBSSSCHOOL <dilippawar2207@gmail.com>
2026-06-26 21:06:01 +0530

    Add files via upload

--- health.js
+++ health.js
@@ -2,6 +2,6 @@ export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   return res.status(200).json({
     status: true,
-    message: "WhatsBot Proxy API is working"
+    message: "School ERP WhatsBot Proxy API is working"
   });
 }
