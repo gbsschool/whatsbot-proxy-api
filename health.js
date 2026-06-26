2ba7b2bf905a112913ec8aa3defca53001741080
GBSSSCHOOL <dilippawar2207@gmail.com>
2026-06-26 21:19:34 +0530

    Update health.js

--- health.js
+++ health.js
@@ -1,7 +1,1 @@
-export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json({
-    status: true,
-    message: "WhatsBot Proxy API is working"
-  });
-}
+
